561fcf4f770865d697ae39221ae050da5a5cb8a9
pinguinoadelia <gianmarco.delaurentiis@stud.unifi.it>
2026-05-29 15:44:11 +0200

    Prima implementazione esoscheletro main.rs

--- certification_authority/src/main.rs
+++ certification_authority/src/main.rs
@@ -1,6 +1,49 @@
+#![forbid(unsafe_code)]
+
 pub mod config;
 mod small_server;
 
-fn main() {
-    println!("Hello world, from the CA!");
+use anyhow::{Context, Result};
+use clap::Parser;
+use config::Config;
+use small_server::SmallServer;
+use std::path::PathBuf;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+#[derive(Parser, Debug)]
+#[command(name = "STSS certification authority")]
+#[command(version = "0.1.0")]
+#[command(about = "Minimal Certification Authority Skeleton for STSS")]
+struct Cli{
+    
+    /// Path to the CA TOML configuration file.
+    #[arg(short, long, value_name = "FILE")]
+    config: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+       .with_env_filter(EnvFilter::from_default_env().add_directive("info".parse()?))
+       .init();
+
+    let cli = Cli::parse();
+
+    info!(
+        config_path = %cli.config.display(),
+        "starting STSS Certification Authority"
+    );
+
+    let config = Config::from_file(cli.config)
+        .context("failed to read the CA configuration file")?;
+
+    let context = config
+        .to_context()
+        .context("failed to build the CA runtime context")?;
+
+    SmallServer::build(context)
+        .run()
+        .await()
+        .context("Certification Authority stopped with an error")
 }
